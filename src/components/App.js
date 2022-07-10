58aa9802862b67c8a4e4bac0ec50b4993d6f3b1c
KamyabTabani <k.tabani82@gmail.com>
2022-07-10 23:07:33 +0430

    Header Full

--- src/components/App.js
+++ src/components/App.js
@@ -7,42 +7,42 @@ import "../sass/main.css";
 import TextCardContainer from "./TextCardContainer";
 
 const App = () => {
-    const headerTitle = "Your Health Is Our top Priority";
-    const headerText = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet assumenda debitis earum est " +
-        "explicabo fuga id minus neque, nihil nobis odio sequi sit ut vel vero! Aperiam debitis labore voluptate?";
-    const headerDetails = [
-        {
-            num: 216,
-            text: "Healthcare Facility"
-        },
-        {
-            num: 423,
-            text: "Professional Doctor"
-        },
-        {
-            num: 669,
-            text: "Staff all around the world"
-        }
-    ];
+  const headerTitle = "Your Health Is Our top Priority";
+  const headerText =
+    "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet assumenda debitis earum est " +
+    "explicabo fuga id minus neque, nihil nobis odio sequi sit ut vel vero! Aperiam debitis labore voluptate?";
+  const headerDetails = [
+    {
+      num: 216,
+      text: "Healthcare Facility",
+    },
+    {
+      num: 423,
+      text: "Professional Doctor",
+    },
+    {
+      num: 669,
+      text: "Staff all around the world",
+    },
+  ];
 
-    const smallHeading1="Healthcare Facilities";
-    const bigHeading1="Our Last Healthcare ";
+  const smallHeading1 = "Healthcare Facilities";
+  const bigHeading1 = "Our Last Healthcare ";
 
-    return (
-        <>
-            <div className="container-fluid">
-                <div className="mt-3">
-                    <TopBar />
-                </div>
-            </div>
-            <div className={"container"}>
-            <Header title={headerTitle} text={headerText} details={headerDetails}/>
-            </div>
-            <Heading smallTitle={smallHeading1} bigTitle={bigHeading1}/>
-            <TextCardContainer/>
-        </>
-
-    );
-}
+  return (
+    <>
+      <div className="container-fluid">
+        <div className="mt-3">
+          <TopBar />
+        </div>
+      </div>
+      <div className={"container-fluid"}>
+        <Header title={headerTitle} text={headerText} details={headerDetails} />
+      </div>
+      <Heading smallTitle={smallHeading1} bigTitle={bigHeading1} />
+      <TextCardContainer />
+    </>
+  );
+};
 
 export default App;
--- src/components/TextCard.js
+++ src/components/TextCard.js
@@ -4,13 +4,13 @@ class TextCard extends React.Component {
     render() {
 
         return (
-            <div className={`card card--${this.props.theme}`}>
-                <div className={"card__top"}></div>
-                <div className={"card__body"}>
-                    <h6 className={"card__title"}><strong>{this.props.title} </strong><i
+            <div className={`cardtext cardtext--${this.props.theme}`}>
+                <div className={"cardtext__top"}></div>
+                <div className={"cardtext__body"}>
+                    <h6 className={"cardtext__title"}><strong>{this.props.title} </strong><i
                         className={`flag flag-${this.props.flag}`}></i></h6>
-                    <p className={"card__text"}>{this.props.text}</p>
-                    <a href="src/components/TextCard#" className={"card__badge"}>
+                    <p className={"cardtext__text"}>{this.props.text}</p>
+                    <a href="src/components/TextCard#" className={"cardtext__badge"}>
                         <strong>
                             <span>&bull; </span>
                             {this.props.badge}
