0119cc81e336489a6bddfbf8483b81b2b3de1bd6
KamyabTabani <k.tabani82@gmail.com>
2022-07-15 23:04:50 +0430

    Theme Switcher

--- src/components/ToggleButton.js
+++ src/components/ToggleButton.js
@@ -1,1 +1,28 @@
 import React from "react";
+import ThemeContext from "../context/ThemeContext";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+
+class ToggleButton extends React.Component {
+  render() {
+    return (
+      <ThemeContext.Consumer>
+        {({toggleTheme }) => (
+          <button
+            className="btn rounded-circle shadow-lg"
+            onClick={() => toggleTheme()}
+          >
+            <div
+              className="d-flex justify-content-center align-items-center"
+              style={{ width: "15px", height: "20px" }}
+            >
+              <DarkModeOutlinedIcon />
+            </div>
+          </button>
+        )}
+      </ThemeContext.Consumer>
+    );
+  }
+}
+
+export default ToggleButton;
